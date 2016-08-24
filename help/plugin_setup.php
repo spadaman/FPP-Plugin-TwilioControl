7e6a06f63a5b607d18b2b3dc5ee275ed3a9fce7f
LightsOnHudson <brbshaver@gmail.com>
2016-08-24 12:11:51 -0600

    First good release

--- help/plugin_setup.php
+++ help/plugin_setup.php
@@ -5,3 +5,8 @@
 <p>
 Welcome to the Twilio Control tools plugin Help page
 
+<p>You will need to create an account at Twilio
+
+<p>The account credentials that you have will then be used to configure this plugin
+
+
