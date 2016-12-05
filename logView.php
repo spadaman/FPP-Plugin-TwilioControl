e0c9011d9e9396f452e338c1c92b44ac1646e690
LightsOnHudson <brbshaver@gmail.com>
2016-12-05 10:35:23 -0700

    log viewer

--- logView.php
+++ logView.php
@@ -108,36 +108,22 @@ if (file_exists($pluginConfigFile))
 	$TSMS_body = "";
 	$TSMS_BODY_CONTAINED_HEX = false;
 	
-	if (isset($_GET['ajax'])) {
-		session_start();
-		$handle = fopen($logFile, 'r');
-		if (isset($_SESSION['offset'])) {
-			$data = stream_get_contents($handle, -1, $_SESSION['offset']);
-			echo nl2br($data);
-		} else {
-			fseek($handle, 0, SEEK_END);
-			$_SESSION['offset'] = ftell($handle);
-		}
-		exit();
-	}
-	?>
-	<!doctype html>
-	<html lang="en">
-	<head>
-	  <meta charset="UTF-8">
-	  <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-	  <script src="http://creativecouple.github.com/jquery-timing/jquery-timing.min.js"></script>
-	  <script>
-	  $(function() {
-	    $.repeat(1000, function() {
-	      $.get($_SERVER['SERVER_NAME']?>/plugin.php?plugin=<?echo $pluginName;?>&page=logView.php'?ajax', function(data) {
-	        $('#tail').append(data);
-	      });
-	    });
-	  });
-	  </script>
-	</head>
-	<body>
-	  <div id="tail">Starting up...</div>
-	</body>
-	</html>
+	session_start();
+$_SESSION['tailSize'] = filesize($logFile);
+if($_SESSION['tailPrevSize'] == '' || $_SESSION['tailPrevSize'] > $_SESSION['tailSize'])
+{
+      $_SESSION['tailPrevSize'] = $_SESSION['tailSize'];
+}
+$tailDiff = $_SESSION['tailSize'] - $_SESSION['tailPrevSize'];
+$_SESSION['tailPrevSize'] = $_SESSION['tailSize'];
+
+/* Include your own security checks (valid user, etc) if required here */
+
+
+$handle = popen("tail -c ".$tailDiff." ".$logFile." 2>&1", 'r');
+while(!feof($handle)) {
+    $buffer = fgets($handle);
+    echo "$buffer";
+    flush(); 
+}
+pclose($handle);
