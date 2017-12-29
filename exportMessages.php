23d5bad6569c958ae7ce542d964596217700edb2
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-12-29 09:17:49 -0700

    export

--- exportMessages.php
+++ exportMessages.php
@@ -1,23 +1,14 @@
 <?php
 header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=data.csv');
+header('Content-Disposition: attachment; filename=TwilioMessages.csv');
 $pluginName = "TwilioControl";
-$tmpDownloadFilename = "/tmp/messages.csv";
 $Plugin_DBName = "/home/fpp/media/config/FPP.".$pluginName.".db";
 $db = new SQLite3($Plugin_DBName) or die('Unable to open database');
-$tmpData = "";
 // create a file pointer connected to the output stream
 $output = fopen('php://output', 'w');
 $messagesQuery = "SELECT * FROM messages WHERE pluginName = '".$pluginName."'  ORDER BY timestamp DESC";
 $messagesResult = $db->query($messagesQuery) or die('Query failed');
-// loop over the rows, outputting them
+//loop over the rows, outputting them
 while ($row = $messagesResult->fetchArray()) fputcsv($output, $row);
-	//	while ($row = $messagesResult->fetchArray()) { $tmpData .= implode(array_values($row), ",") . "\n"; }
-//	file_put_contents($tmpDownloadFilename, $tmpData);
-	//header("Content-Disposition: attachment; filename=\"" . $tmpDownloadFilename. "\"");
-	//header("Content-Type: text/csv");
-	//header("Content-Length: " . filesize($tmpDownloadFilename));
-	//header("Connection: close");
-	//readfile($tmpDownloadFilename);
-	//exit;
+exit;
 ?>
