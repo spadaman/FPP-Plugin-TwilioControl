124c33c435514c144630637e0b0db84bd5474bb7
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-12-29 08:29:02 -0700

    Export

--- exportMessages.php
+++ exportMessages.php
@@ -1,5 +1,4 @@
-<?php $skipJSsettings = 1; ?>
-<?
+<?php
 $pluginName = "TwilioControl";
 $tmpDownloadFilename = "/tmp/messages.csv";
 $Plugin_DBName = "/home/fpp/media/config/FPP.".$pluginName.".db";
@@ -11,12 +10,6 @@ header('Content-Disposition: attachment; filename=data.csv');
 $output = fopen('php://output', 'w');
 $messagesQuery = "SELECT * FROM messages WHERE pluginName = '".$pluginName."'  ORDER BY timestamp DESC";
 $messagesResult = $db->query($messagesQuery) or die('Query failed');
-// output the column headings
-//fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
-// fetch the data
-//mysql_connect('localhost', 'username', 'password');
-//mysql_select_db('database');
-//$rows = mysql_query('SELECT field1,field2,field3 FROM table');
 // loop over the rows, outputting them
 while ($row = $messagesResult->fetchArray()) fputcsv($output, $row);
 	//	while ($row = $messagesResult->fetchArray()) { $tmpData .= implode(array_values($row), ",") . "\n"; }
